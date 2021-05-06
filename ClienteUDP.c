10555b805fc1a770f212da948f88e873f18817b2
GMoreira-Tro <guilherme.dsmoreira@gmail.com>
2021-05-06 18:02:28 -0300

    aff

---
+++ ClienteUDP.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+
+#define DEFAULT_PROTOCOL 0
+#define INVALID -1
+#define ECHOMAX 255
+
+int main(int argc, char* argv[])
+{
+    //Acho extremamente feio um array de char, prefereria mil vezes um ponteiro de char. Mas, porém, todavia, entretanto,
+    // eu entendi porque char* é extremamente inseguro de ler em runtime.
+    char message[ECHOMAX];
+	struct sockaddr_in serverTarget = {0};
+	char* serverIP;
+
+	int socketID = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+	if(socketID == INVALID)
+	{
+		perror("failed to create socket");
+		exit(EXIT_FAILURE);
+	}
+
+	serverTarget.sin_family = AF_INET;
+	serverIP = argv[1];
+	serverTarget.sin_addr.s_addr = inet_addr(serverIP);
+	serverTarget.sin_port = htons(6000);
+
+	do
+	{
+        fgets(message, sizeof(message), stdin);
+        message[strlen(message)- 1] = '\0';
+        printf("%s - %d\n", message, (int)strlen(message));
+
+        sendto(socketID, message, strlen(message),
+		0, (const struct sockaddr *)&serverTarget, sizeof(serverTarget));
+
+	}
+	while (strcmp(message, "Luiz gay"));
+
+    close(socketID);
+    return 0;
+}
---
+++ ServidorUDP.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <string.h>
+
+#define DEFAULT_PROTOCOL 0
+#define INVALID -1
+#define ECHOMAX 255
+
+int main(int argc, char* argv[])
+{
+    char buffer[ECHOMAX];
+
+	struct sockaddr_in serverSender = {0};
+	struct sockaddr_in serverFrom;
+
+	int sizeOfServerSender = sizeof(serverSender);
+
+	int socketID = socket(AF_INET, SOCK_DGRAM, DEFAULT_PROTOCOL);
+	if (socketID == INVALID)
+	{
+		perror("failed to create socket");
+		exit(EXIT_FAILURE);
+	}
+
+	serverSender.sin_family = AF_INET;
+	serverSender.sin_port = htons(6000);
+	serverSender.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if(bind(socketID, (const struct sockaddr *)&serverSender,
+		sizeof(serverSender)) == INVALID)
+	{
+		perror("failed to bind");
+		close(socketID);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+        do
+        {
+            memset(buffer, 0, strlen(buffer));
+
+            recvfrom(socketID, buffer, sizeof(buffer), DEFAULT_PROTOCOL, (struct sockaddr*)&serverFrom,
+            &sizeOfServerSender);
+
+            printf("%s\n", buffer);
+
+        }
+        while(strcmp(buffer, "Luiz gay"));
+	}
+
+	close(socketID);
+    return 0;
+}
